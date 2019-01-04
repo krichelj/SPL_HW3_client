c48731e7c1f5bbb63a91e2904a7c7b9b5095a72c
Shay Kricheli <skricheli2@gmail.com>
2019-01-04 21:46:25 +0200

    changes

--- src/ClientToServerEncoder.cpp
+++ src/ClientToServerEncoder.cpp
@@ -51,57 +51,28 @@ void ClientToServerEncoder::operator()() {
 
         else if (command == "FOLLOW"){
 
-            shortToBytesAndSend(4);
-            char followOrUnfollow[1];
-            currentConnectionHandler->sendLine(commandFromUser[1]); // send the follow/unfollow
-            currentConnectionHandler->sendLine(commandFromUser[2]); // send the NumOfUsers
-            for (int i=1; i<=stoi(commandFromUser[2]); i++)
-                currentConnectionHandler->sendLine(commandFromUser[i+2]); // send the NumOfUsers
+            if (commandFromUser.size() > 2){ // check that there are at least 3 arguments
+
+                shortToBytesAndSend(4); // send the opCode
+                shortToBytesAndSend(static_cast<short>(stoi(commandFromUser[1]))); // send the followOrUnfollow
+
+                auto numOfUsers = (short) stoi(commandFromUser[2]);
+                shortToBytesAndSend(numOfUsers); // send the numOfUsers
+
+                // send all the user names
+                for (unsigned int i = 0; i < numOfUsers; i++)
+                    currentConnectionHandler->sendLine(commandFromUser[i + 3]); // send the NumOfUsers
+            }
         }
-
-
-
-
-
-
-            /*if (!currentConnectionHandler.sendLine(inputLine)) {
-                cout << "Disconnected. Exiting...\n" << endl;
-                break;
-            }
-            // connectionHandler.sendLine(line) appends '\n' to the message. Therefor we send len+1 bytes.
-            cout << "Sent " << length+1 << " bytes to server" << endl;
-
-            // We can use one of three options to read data from the server:
-            // 1. Read a fixed number of characters
-            // 2. Read a line (up to the newline character using the getline() buffered reader
-            // 3. Read up to the null character
-            string answer;
-            // Get back an answer: by using the expected number of bytes (len bytes + newline delimiter)
-            // We could also use: connectionHandler.getline(answer) and then get the answer without the newline char at the end
-            if (!currentConnectionHandler.getLine(answer)) {
-                cout << "Disconnected. Exiting...\n" << endl;
-                break;
-            }
-
-            length= static_cast<int>(answer.length());
-            // A C string must end with a 0 char delimiter.  When we filled the answer buffer from the socket
-            // we filled up to the \n char - we must make sure now that a 0 char is also present. So we truncate last character.
-            answer.resize(static_cast<unsigned long>(length - 1));
-            cout << "Reply: " << answer << " " << length << " bytes " << endl << endl;
-            if (answer == "bye") {
-                cout << "Exiting...\n" << endl;
-                break;
-            }*/
-
     }
 }
 
 void ClientToServerEncoder::shortToBytesAndSend(short number) {
 
-    char opCodeByteArray[2];
-    opCodeByteArray[0] = static_cast<char>((number >> 8) & 0xFF);
-    opCodeByteArray[1] = static_cast<char>(number & 0xFF);
-    currentConnectionHandler->sendBytes(opCodeByteArray,2);
+    char ByteArray[2];
+    ByteArray[0] = static_cast<char>((number >> 8) & 0xFF);
+    ByteArray[1] = static_cast<char>(number & 0xFF);
+    currentConnectionHandler->sendBytes(ByteArray,2);
 }
 
 void ClientToServerEncoder::terminate() {
