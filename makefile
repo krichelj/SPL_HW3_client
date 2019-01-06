47fec28a53ed875751ee4ae12c79cc0b5a03f8fb
Shay Kricheli <skricheli2@gmail.com>
2019-01-06 18:04:01 +0200

    changes

--- makefile
+++ makefile
@@ -1,21 +1,14 @@
-# define some Makefile variables for the compiler and compiler flags
-# to use Makefile variables later in the Makefile: $()
 CC = g++
 CFLAGS  = -g -Wall -Weffc++ -std=c++11
 LFLAGS  = -L/usr/lib -lboost_system -pthread
 
-# All Targets
 all: BGSclient
 
-BGSclient: bin/BGSClient.o bin/ConnectionHandler.o bin/ClientToServerEncoder.o bin/ServerToClientDecoder.o
-	@echo 'Building target: BGSclient'
-	@echo 'Invoking: C++ Linker'
-	$(CC) -o bin/BGSClient bin/BGSClient.o bin/ConnectionHandler.o bin/ClientToServerEncoder.o bin/ServerToClientDecoder.o $(LFLAGS)
-	@echo 'Finished building target: BGSclient'
-	@echo ' '
+BGSclient: bin/BGSclient.o bin/ConnectionHandler.o bin/ClientToServerEncoder.o bin/ServerToClientDecoder.o
+	$(CC) -o bin/BGSclient bin/BGSclient.o bin/ConnectionHandler.o bin/ClientToServerEncoder.o bin/ServerToClientDecoder.o $(LFLAGS)
 
-bin/BGSClient.o: src/BGSClient.cpp
-	$(CC) $(CFLAGS) -c -Iinclude -o bin/BGSClient.o src/BGSClient.cpp
+bin/BGSclient.o: src/BGSclient.cpp
+	$(CC) $(CFLAGS) -c -Iinclude -o bin/BGSclient.o src/BGSclient.cpp
 
 bin/ConnectionHandler.o: src/ConnectionHandler.cpp
 	$(CC) $(CFLAGS) -c -Iinclude -o bin/ConnectionHandler.o src/ConnectionHandler.cpp
@@ -26,6 +19,5 @@ bin/ClientToServerEncoder.o: src/ClientToServerEncoder.cpp
 bin/ServerToClientDecoder.o: src/ServerToClientDecoder.cpp
 	$(CC) $(CFLAGS) -c -Iinclude -o bin/ServerToClientDecoder.o src/ServerToClientDecoder.cpp
 
-#Clean the build directory
 clean: 
 	rm -f bin/*
--- src/ClientToServerEncoder.cpp
+++ src/ClientToServerEncoder.cpp
@@ -1,55 +1,60 @@
 #include <ClientToServerEncoder.h>
-#include <boost/algorithm/string.hpp>
 
 using namespace std;
 
 // constructor
+
 ClientToServerEncoder::ClientToServerEncoder(ConnectionHandler* currentConnectionHandler, mutex* logoutMutex, condition_variable* conditionVariable):
         currentConnectionHandler(currentConnectionHandler), logoutMutex(logoutMutex), conditionVariable(conditionVariable), shouldTerminate(false){
 
 }
 
 // thread runner method
+
 void ClientToServerEncoder::operator()() {
 
     while (!shouldTerminate){
 
-        //From here we will see the rest of the echo client implementation:
         const short inputBufferSize = 1024;
         char buffer[inputBufferSize];
-        cin.getline(buffer, inputBufferSize); // inputs commands from the user
-        string inputLine(buffer); // creates a line from the commands
+        vector<string> commandFromUser; // a vector of strings for the keyboard input from the user
 
-        vector<string> commandFromUser;
-        boost::split(commandFromUser,inputLine,boost::is_any_of(" "));
-        string command = commandFromUser[0];
+        cin.getline (buffer, inputBufferSize); // inputs commands from the user
+        string inputLine (buffer); // create a line from the commands
+        istringstream stringStream (inputLine); // create a string stream from the input line
 
-        if (command == "REGISTER"){
+        // break down the string stream into discrete strings
+        for(string currentString ; stringStream >> currentString ;)
+            commandFromUser.push_back(currentString);
+
+        string commandName = commandFromUser[0]; // record the commandName name
+
+        if (commandName == "REGISTER"){
 
             shortToBytesAndSend(1); // change the array of shorts to an array of bytes and send the opcode
             currentConnectionHandler->sendLine(commandFromUser[1]); // send the username
             currentConnectionHandler->sendLine(commandFromUser[2]); // send the password
         }
 
-        else if (command == "LOGIN"){
+        else if (commandName == "LOGIN"){
 
             shortToBytesAndSend(2);
             currentConnectionHandler->sendLine(commandFromUser[1]); // send the username
             currentConnectionHandler->sendLine(commandFromUser[2]); // send the password
         }
 
-        else if (command == "LOGOUT"){
+        else if (commandName == "LOGOUT"){
 
             shortToBytesAndSend(3);
             unique_lock<mutex> logoutLock(*logoutMutex);
             conditionVariable->wait(logoutLock);
         }
 
-        else if (command == "FOLLOW"){
+        else if (commandName == "FOLLOW"){
 
             if (commandFromUser.size() > 2){ // check that there are at least 3 arguments
 
-                shortToBytesAndSend(4); // send the opCode
+                shortToBytesAndSend(4);
                 shortToBytesAndSend(static_cast<short>(stoi(commandFromUser[1]))); // send the followOrUnfollow
 
                 auto numOfUsers = (short) stoi(commandFromUser[2]);
@@ -61,44 +66,44 @@ void ClientToServerEncoder::operator()() {
             }
         }
 
-        else if (command == "POST"){
+        else if (commandName == "POST"){
 
             string postContent;
 
-            if(commandFromUser.size() > 1)  // checks that there's content in the post
+            if(commandFromUser.size() > 1)  // check there's content in the post
                 postContent = commandFromUser[1];
 
             // add all of the post
             for (unsigned int i = 2; i < commandFromUser.size(); i++)
                 postContent += " " + commandFromUser[i];
 
-            shortToBytesAndSend(5); // send the op code
-            currentConnectionHandler->sendLine(postContent);
+            shortToBytesAndSend(5);
+            currentConnectionHandler->sendLine(postContent); // send the post content
         }
 
-        else if (command == "PM"){
+        else if (commandName == "PM"){
 
             if (commandFromUser.size() > 1) { //checks if message is not empty
 
-                string content;
+                string pmContent;
 
                 if (commandFromUser.size() > 2)
-                    content = commandFromUser[2];
+                    pmContent = commandFromUser[2];
 
                 for (unsigned int i = 3; i < commandFromUser.size(); i++) {
-                    content += " " + commandFromUser[i];
+                    pmContent += " " + commandFromUser[i];
                 }
-                shortToBytesAndSend(6); // send the op code
+                shortToBytesAndSend(6);
                 currentConnectionHandler->sendLine(commandFromUser[1]);
-                currentConnectionHandler->sendLine(content);
+                currentConnectionHandler->sendLine(pmContent);
             }
         }
 
-        else if (command == "USERLIST")
+        else if (commandName == "USERLIST")
             shortToBytesAndSend(7); // send the op code
 
 
-        else if (command == "STAT"){
+        else if (commandName == "STAT"){
 
             if (commandFromUser.size() == 2){
 
